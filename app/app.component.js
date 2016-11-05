db3a20bab3eaa17576234d8ac993938c7a034edd
Camilo <camilo@twinatom.com>
2016-11-05 12:21:10 +0100

    Initial commit

---
+++ app/app.component.js
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: `
+    <StackLayout>
+    <Label text="Tap the button" class="title"></Label>
+    </StackLayout>`
+})
+export class AppComponent {
+
+}
---
+++ app/main.js
@@ -0,0 +1,12 @@
+// this import should be first in order to load some required settings (like globals and reflect-metadata)
+import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
+import { NgModule } from "@angular/core"; 
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    bootstrap: [AppComponent],
+    imports: [NativeScriptModule],
+})
+class AppComponentModule {}
+
+platformNativeScriptDynamic().bootstrapModule(AppComponentModule);
